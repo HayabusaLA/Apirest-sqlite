2332d40b9a8c4d317cb3ccd87778b0fb92e57339
HayabusaLA <105665909+HayabusaLA@users.noreply.github.com>
2024-09-07 14:38:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-var express = require("express")
-var app = express()
-require('dotenv').config({path: __dirname + '/.env'})
-var db = require("./database.js")
-const { getUsers, getUser, createUser, updateUser, deleteUser } = require('./controllers/userController')
+const express = require("express");
+const app = express();
+require('dotenv').config({path: __dirname + '/.env'});
+const db = require("./database.js");
+const { getUsers, getUser, createUser, updateUser, deleteUser } = require('./controllers/userController');
+const bodyParser = require("body-parser");
 
-var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
-var products = require('./routes/product.js')
+const port = process.env.PORT || 8080;
 
-
-
+const products = require('./routes/product.js');
 
 // products endpoints
-app.use('/api/product', products)
-
+app.use('/api/product', products);
 
 // user endpoints
 app.get("/api/users", (req, res, next) => {
-   getUsers(req, res)
+   getUsers(req, res);
 });
 
 app.get("/api/user/:id", (req, res, next) => {
-   getUser(req, res)
+   getUser(req, res);
 });
 
 app.post("/api/user/", (req, res, next) => {
-    createUser(req, res)
-})
+    createUser(req, res);
+});
 
 app.patch("/api/user/:id", (req, res, next) => {
-   updateUser(req, res)
-})
+   updateUser(req, res);
+});
 
 app.delete("/api/user/:id", (req, res, next) => {
-  deleteUser
-})
+   deleteUser(req, res);
+});
 
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 
 // Start server
